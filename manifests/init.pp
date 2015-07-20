1155f9ad0d89908c095c274be843a5176f335880
Siddhu Warrier <siwarrie@cisco.com>
2015-07-20 23:05:31 +0100

    Install vim with lua using Brew before checking out luan vimfiles

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,18 @@
-
 class luanvim {
+
 	include boxen::config
-	$home_dir="/Users/${::boxen_user}/.vim"
-	repository {
+  require homebrew
+  $home_dir="/Users/${::boxen_user}/.vim"
+	
+  package { 'vim':
+    provider => homebrew,
+    install_options => ['--with-lua']
+  } -> 
+  repository {
 	  $home_dir:
 	    source   => 'luan/vimfiles', 
 	    provider => 'git';
-	} ->
+	} ~>
 	exec { 'install luan vimfiles':
 		command => "$home_dir/install"
 	}
